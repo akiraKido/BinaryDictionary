ed74cbe5490e3190980b794f67051f06be83ab2d
Akira Kido <achts.y@gmail.com>
2019-07-26 13:51:13 +0900

    initial commit

---
+++ BinaryDictionary.Tests/Tests.cs
@@ -0,0 +1,106 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace BinaryDictionary.Tests
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(1, 1, true)]
+        [InlineData(1, 2, false)]
+        [InlineData(31, 31, true)]
+        [InlineData(29, 31, false)]
+        [InlineData(32, 32, true)]
+        public void TestRegisterAndContains(int input, int check, bool expected)
+        {
+            var binaryDictionary = new BinaryDictionary();
+            binaryDictionary.Add(input);
+
+            binaryDictionary.Contains(check).Should().Be(expected);
+        }
+
+        [Fact]
+        public void TestMultipleRegister()
+        {
+            var binaryDictionary = new BinaryDictionary();
+
+            binaryDictionary.Add(1);
+            binaryDictionary.Add(31);
+            binaryDictionary.Add(32);
+
+            binaryDictionary.Contains(1).Should().Be(true);
+            binaryDictionary.Contains(31).Should().Be(true);
+            binaryDictionary.Contains(32).Should().Be(true);
+
+            binaryDictionary.Size.Should().Be(64);
+            
+            for (int i = 0; i < binaryDictionary.Size; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        binaryDictionary.Contains(1).Should().Be(true);
+                        continue;
+                    case 31:
+                        binaryDictionary.Contains(31).Should().Be(true);
+                        continue;
+                    case 32:
+                        binaryDictionary.Contains(32).Should().Be(true);
+                        continue;
+                    default:
+                        binaryDictionary.Contains(i).Should().Be(false);
+                        break;
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(1, 1, false)]
+        public void TestRemove(int input, int remove, bool expected)
+        {
+            var binaryDictionary = new BinaryDictionary();
+            binaryDictionary.Add(input);
+            binaryDictionary.Remove(remove);
+
+            binaryDictionary.Contains(remove).Should().Be(expected);
+        }
+
+        [Fact]
+        public void TestRemoveGreaterThanSize()
+        {
+            var binaryDictionary = new BinaryDictionary();
+            binaryDictionary.Invoking(b => b.Remove(100))
+                .Should().NotThrow();
+        }
+        [Fact]
+        public void TestContainsGreaterThanSize()
+        {
+            var binaryDictionary = new BinaryDictionary();
+            binaryDictionary.Contains(100).Should().Be(false);
+        }
+
+        [Fact]
+        public void TestRegisterThrow()
+        {
+            var binaryDictionary = new BinaryDictionary();
+            binaryDictionary.Invoking(b => b.Add(-1))
+                .Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void TestContainsThrow()
+        {
+            var binaryDictionary = new BinaryDictionary();
+            binaryDictionary.Invoking(b => b.Contains(-1))
+                .Should().Throw<ArgumentException>();
+        }
+        [Fact]
+        public void TestRemoveThrow()
+        {
+            var binaryDictionary = new BinaryDictionary();
+            binaryDictionary.Invoking(b => b.Remove(-1))
+                .Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ BinaryDictionary/BinaryDictionary.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace BinaryDictionary
+{
+    public class BinaryDictionary
+    {
+        private int[] dictionaries;
+
+        public BinaryDictionary()
+        {
+            dictionaries = new int[1];
+        }
+
+        public void Add(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("index must be positive");
+            }
+        
+            CheckDictionarySize(index);
+
+            var valueOffset = index % 32;
+            var orValue = 1 << valueOffset;
+        
+            var dictionaryIndex = index / 32;
+            dictionaries[dictionaryIndex] |= orValue;
+        }
+
+        public bool Contains(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("index must be positive");
+            }
+        
+            if (index >= Size)
+            {
+                return false;
+            }
+        
+            var dictionaryIndex = index / 32;
+            var valueOffset = index % 32;
+            var checkOrValue = 1 << valueOffset;
+
+            return (checkOrValue & dictionaries[dictionaryIndex]) != 0;
+        }
+
+        public void Remove(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("index must be positive");
+            }
+
+            if (index > Size)
+            {
+                return;
+            }
+        
+            var valueOffset = index % 32;
+            var orValue = 1 << valueOffset;
+        
+            var dictionaryIndex = index / 32;
+            dictionaries[dictionaryIndex] ^= orValue;
+        }
+
+        public int Size => dictionaries.Length * 32;
+
+        private void CheckDictionarySize(int index)
+        {
+            var necessarySize = index / 32;
+            if (necessarySize < dictionaries.Length)
+            {
+                return;
+            }
+
+            var doubleSize = dictionaries.Length * 2;
+            var newArray = necessarySize > doubleSize
+                ? new int[necessarySize]
+                : new int[doubleSize];
+
+            Array.Copy(dictionaries, 0, newArray, 0, dictionaries.Length);
+            dictionaries = newArray;
+        }
+    }
+}
